0c58f412eb9fcbd1c4c6ea7740e7aa1c533f41b6
hailaz <739476267@qq.com>
2024-10-28 16:47:10 +0800

    ci: fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,4 +1,4 @@
-const LATEST_VERSION_LABEL = "Latest";
+const LATEST_VERSION_LABEL = 'Latest';
 
 import type * as Preset from '@docusaurus/preset-classic';
 import type { Config } from '@docusaurus/types';
@@ -8,10 +8,11 @@ import { themes as prismThemes } from 'prism-react-renderer';
 // https://docusaurus.io/docs/3.2.1/api/docusaurus-config
 const config: Config = {
   title: 'GoFrame官网 - 类似PHP-Laravel, Java-SpringBoot的Go企业级开发框架',
-  tagline: 'GoFrame is a modular, powerful, high-performance and enterprise-class application development framework of Golang.',
+  tagline:
+    'GoFrame is a modular, powerful, high-performance and enterprise-class application development framework of Golang.',
   favicon: '/img/favicon.ico',
   url: 'https://goframe.org/',
-  baseUrl: '/',
+  baseUrl: '/gf-site/',
   trailingSlash: false,
   organizationName: 'gogf',
   projectName: 'gf',
@@ -26,16 +27,27 @@ const config: Config = {
     [
       'classic',
       {
+        // Will be passed to @docusaurus/plugin-content-docs (false to disable)
         docs: {
           sidebarPath: './sidebars.ts',
-          lastVersion: "current",
+          lastVersion: 'current',
           // https://docusaurus.io/docs/versioning
           versions: {
-              current: {
-                  label:  LATEST_VERSION_LABEL,
-              },
+            current: {
+              label: LATEST_VERSION_LABEL,
+            },
           },
+          // 编辑当前页面的配置
+          editUrl: 'https://github.com/gogf/gf-site/blob/main/',
         },
+        // Will be passed to @docusaurus/plugin-content-blog (false to disable)
+        blog: {},
+        // Will be passed to @docusaurus/plugin-content-pages (false to disable)
+        pages: {
+          showLastUpdateTime: true,
+          showLastUpdateAuthor: true,
+        },
+        // Will be passed to @docusaurus/theme-classic.
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -52,7 +64,7 @@ const config: Config = {
         language: 'zh',
       },
     ],
-    
+
     // 快速开始
     [
       '@docusaurus/plugin-content-docs',
@@ -90,11 +102,10 @@ const config: Config = {
     [
       '@docusaurus/plugin-content-docs',
       {
-        id: "help",
-        path: "help",
-        routeBasePath: "help",
+        id: 'help',
+        path: 'help',
+        routeBasePath: 'help',
         sidebarPath: './sidebars.ts',
-
       },
     ],
     // 支持我们
@@ -128,11 +139,11 @@ const config: Config = {
       },
       items: [
         {
-          "type": "docSidebar",
-          "sidebarId": "tutorialSidebar",
-          "docsPluginId": "quick",
-          "position": "left",
-          "label": "快速开始"
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          docsPluginId: 'quick',
+          position: 'left',
+          label: '快速开始',
         },
         {
           position: 'left',
@@ -140,81 +151,81 @@ const config: Config = {
           items: [
             {
               label: '框架设计',
-              to: '/docs/框架设计'
+              to: '/docs/框架设计',
             },
             {
               label: '开发工具',
-              to: '/docs/开发工具'
+              to: '/docs/开发工具',
             },
             {
               label: '组件手册',
-              to: '/docs/核心组件'
+              to: '/docs/核心组件',
             },
             {
               label: '微服务开发',
-              to: '/docs/微服务开发'
+              to: '/docs/微服务开发',
             },
             {
               label: 'WEB服务开发',
-              to: '/docs/WEB服务开发'
+              to: '/docs/WEB服务开发',
             },
             {
               label: '服务可观测性',
-              to: '/docs/服务可观测性'
+              to: '/docs/服务可观测性',
             },
-          ]
+          ],
         },
         {
-          "type": "docSidebar",
-          "sidebarId": "tutorialSidebar",
-          "docsPluginId": "faq",
-          "position": "left",
-          "label": "常见问题"
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          docsPluginId: 'faq',
+          position: 'left',
+          label: '常见问题',
         },
         {
-          "type": "docSidebar",
-          "sidebarId": "tutorialSidebar",
-          "docsPluginId": "release",
-          "position": "left",
-          "label": "发布记录"
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          docsPluginId: 'release',
+          position: 'left',
+          label: '发布记录',
         },
 
         {
-          "type": "docSidebar",
-          "sidebarId": "tutorialSidebar",
-          "docsPluginId": "help",
-          "position": "left",
-          "label": "获得帮助"
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          docsPluginId: 'help',
+          position: 'left',
+          label: '获得帮助',
         },
         {
-          "type": "docSidebar",
-          "sidebarId": "tutorialSidebar",
-          "docsPluginId": "supportus",
-          "position": "left",
-          "label": "支持我们"
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          docsPluginId: 'supportus',
+          position: 'left',
+          label: '支持我们',
         },
         {
-          "type": "docSidebar",
-          "sidebarId": "tutorialSidebar",
-          "docsPluginId": "joinus",
-          "position": "left",
-          "label": "加入我们"
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          docsPluginId: 'joinus',
+          position: 'left',
+          label: '加入我们',
         },
         {
-          type: "docsVersionDropdown",
-          position: "right",
+          type: 'docsVersionDropdown',
+          position: 'right',
           dropdownActiveClassDisabled: true,
         },
         {
-          "href": 'https://github.com/gogf/gf/issues',
-          "position": "right",
-          "label": "问题反馈"
+          href: 'https://github.com/gogf/gf/issues',
+          position: 'right',
+          label: '问题反馈',
         },
         {
           href: 'https://github.com/gogf/gf',
           position: 'right',
           className: 'header-github-link',
-        }
+        },
       ],
     },
     // toc目录层级显示设置
