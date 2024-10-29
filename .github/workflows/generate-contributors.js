1acece802dc20069bd5bc4f825401cfb380accc5
oldme <45782393+oldme-git@users.noreply.github.com>
2024-10-29 21:12:24 +0800

    style: 调整贡献值svg

--- .github/workflows/generate-contributors.js
+++ .github/workflows/generate-contributors.js
@@ -7,7 +7,7 @@ const githubUrl = `https://api.github.com/repos/gogf/gf/contributors`
 // 生成 svg 的位置
 const svgPath = "../../static/img/contributors.svg"
 // 单个头像的质量, 1 - 100
-const quality = 1
+const quality = 5
 
 // 单个头像的宽高
 const width = 62, height = 62
