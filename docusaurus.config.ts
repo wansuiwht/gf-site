d7a8c61df13b6897882a38115005e791638d2f9f
hailaz <739476267@qq.com>
2024-11-07 11:35:45 +0800

    fix: 修复toml代码块表头换行问题

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -176,8 +176,9 @@ const config: Config = {
       theme: prismThemes.okaidia,
       darkTheme: prismThemes.dracula,
       defaultLanguage: 'go',
-      additionalLanguages: ['bash', 'javascript', 'yaml', 'ini', 'json'], // 添加语言
+      additionalLanguages: ['bash', 'javascript', 'toml', 'ini'], // 添加语言
       // 默认支持的语言 https://github.com/FormidableLabs/prism-react-renderer/blob/master/packages/generate-prism-languages/index.ts#L9-L23
+      // 默认支持的语言 "markup","jsx","tsx","swift","kotlin","objectivec","js-extras","reason","rust","graphql","yaml","go","cpp","markdown","python","json"
     },
     // 搜索配置
     algolia: {
