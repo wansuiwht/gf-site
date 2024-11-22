1e6dbc5d1f3259144c35776a046179d50160cff3
hailaz <739476267@qq.com>
2024-11-22 15:45:46 +0800

    fix: 优化更新记录脚本

--- .github/workflows/generate-changelog.sh
+++ .github/workflows/generate-changelog.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-echo "---" > docs/docs/changelog.md
-echo "slug: '/docs/changelog'" >> docs/docs/changelog.md
-echo "title: '文档最近更新'" >> docs/docs/changelog.md
-echo "sidebar_position: 99" >> docs/docs/changelog.md
-echo "---" >> docs/docs/changelog.md
-echo "" >> docs/docs/changelog.md
+# 定义文件路径变量
+CHANGELOG_FILE="docs/docs/changelog.md"
+echo "---" > $CHANGELOG_FILE
+echo "slug: '/docs/changelog'" >> $CHANGELOG_FILE
+echo "title: '文档更新记录'" >> $CHANGELOG_FILE
+echo "---" >> $CHANGELOG_FILE
+echo "" >> $CHANGELOG_FILE
 # 获取最近提交记录
-git log -n 100 --pretty=format:"- [%ad](https://github.com/gogf/gf-site/commit/%H) - [%an] - %s" --abbrev-commit --no-merges --date=format:'%m-%d %H:%M' >> docs/docs/changelog.md
+git log -n 100 --pretty=format:"- [%ad](https://github.com/gogf/gf-site/commit/%H) - [%an] - %s" --abbrev-commit --no-merges --date=format:'%m-%d %H:%M' >> $CHANGELOG_FILE
