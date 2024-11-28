df768bcfb9b95cb1ffe0d8b6f5956ed77833580c
John <john@johng.cn>
2024-11-28 11:23:34 +0800

    remove query string for persistance in locale dropdown

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -170,7 +170,6 @@ const config: Config = {
         {
           type: 'localeDropdown',
           position: 'right',
-          queryString: '?persistLocale=true',
         },
         {
           href: 'https://github.com/gogf/gf',
