f9c28bfd03669cdabcd53a5d92c2ea49b0b2f213
John <john@johng.cn>
2024-12-03 22:09:29 +0800

    feat: add marscode ad

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -232,6 +232,10 @@ const config: Config = {
       src: 'https://hm.baidu.com/hm.js?38f38a0626fd7805722db06243cd0fa7',
       async: true,
     },
+    {
+      src: '/ad.js',
+      async: true,
+    },
   ],
 };
 
